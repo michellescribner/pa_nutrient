2478ce4560c4c059a59e96e6d5c356d6add36c87
michellescribner <56173868+michellescribner@users.noreply.github.com>
2021-11-24 12:20:21 -0500

    Add files via upload

--- rawdata/figure1/filtering_final.R
+++ rawdata/figure1/filtering_final.R
@@ -1,7 +1,6 @@
 ### Filtering for Evolution of PA14 in M9 Media Using the Bead Model: no drug populations ###
 
 library(tidyverse)
-library(ggrepel)
 
 setwd("/Users/mrs/Documents/pa14_nodrug/submit/rawdata/figure1")
 
@@ -35,6 +34,7 @@ snps$Frequency <- as.numeric(as.character(snps$Frequency))
 ancestor_snps <- subset(snps,Sample == "PA14 Ancestor")
 snps_noref <- snps[ !(snps$Position %in% ancestor_snps$Position), ]
 nrow(snps_noref)#138
+write.csv(ancestor_snps, file="ancestor_snps.csv")
 
 ### Remove manually filtered mutations
 # multiple mutations within reads, poor mapping likely
@@ -51,7 +51,7 @@ snps_noref <- subset(snps_noref, snps_noref$Gene != "pvcA/ansA")
 snps_noref <- subset(snps_noref, snps_noref$Gene != "PA14_19940")
 snps_noref <- subset(snps_noref, snps_noref$Gene != "PA14_06790")
 
-snps_final <- separate(snps_noref, col = Population, sep = ", ", into = c("day", "pop", "b_p"), remove=FALSE)
+snps_final <- separate(snps_noref, col = Population, sep = ", ", into = c("b_p", "pop", "day"), remove=FALSE)
 # write.csv(snps_final,file=("snps_final.csv"))
 
 # cast data frame - organizing with each mutation as the rows and the frequency of that mutation on a given day as the columns
@@ -61,6 +61,7 @@ write.csv(snps_cast, file="snps_cast.csv")
 
 snps_cast_gene <- snps_final %>%
   pivot_wider(id_cols=c(Gene, Description), names_from = Population, names_sort=TRUE, values_from = Frequency, values_fn = sum, values_fill = 0)
+
 # write.csv(snps_cast_gene, file="snps_cast_gene.csv")
 
 ###############################################
@@ -100,11 +101,13 @@ write.csv(nje_cast, file="nje_cast.csv")
 
 ########################################
 
+library(ggrepel)
+
 ### Figure 1 
 
 # clean NJE to combine with SNPs
-njemap <- nje %>% 
-  select(Population, num, position, freq, annotation, gene, product)
+nje <- as_tibble(nje)
+njemap <- nje %>% select(Population, num, position, freq, annotation, gene, product)
 colnames(njemap) <- c("Sample", "num", "Position", "Frequency", "Annotation", "Gene", "Description")
 njemap$Position <- gsub("= ", "", as.character(njemap$Position))
 njemap$Position <- gsub(" =", "", as.character(njemap$Position))
@@ -158,10 +161,9 @@ c <- snps_final %>%
   scale_shape_manual(name = "Mutation Type", values=c(1, 2, 8, 0))+ 
   scale_color_manual(values = c("quorum sensing" = "#4393C3", "cyclic-di-GMP" = "#339966", "Pf5 prophage" = "#9933FF", "type IV pili" = "#404788FF", "other" = "dark gray")) +
   scale_size(name = "Allele Frequency", range = c(0.25,6), limits = c(0,100)) +
-  scale_x_continuous(limits = c(0,6537648), breaks = c(2e6, 4e6, 6e6),label = c("2,000,000", "4,000,000", "6,000,000"), expand = c(0,0))+
-  theme_minimal()+
+  theme_minimal()+scale_x_continuous(limits = c(0,6537648), breaks = c(2e6, 4e6, 6e6),label = c("2,000,000", "4,000,000", "6,000,000"), expand = c(0,0))+
   theme(legend.position="bottom", legend.box = "vertical", legend.title = element_text(size=12)) + 
-  theme(axis.text=element_text(size=12),axis.title=element_text(size=14))+
+  theme(axis.text=element_text(size=12, color = "black"),axis.title=element_text(size=14))+
   theme(panel.grid.minor = element_blank(), legend.spacing.y = unit(-.1, "in")) +
   xlab("Genome Position (bp)") + ylab("Evolved Population")+
   guides(colour = guide_legend(override.aes = list(size=3)), shape = guide_legend(override.aes = list(size=3)))
@@ -188,12 +190,12 @@ genes <- genes %>%
 genes %>% 
   ggplot(aes(x=Start, y=7, label = Gene.Name)) + 
   geom_rect(aes(xmin=Start, xmax=End, ymin=6, ymax=7), fill="gray")+
-  scale_x_continuous(limits = c(4070000, 4140000), position = "top")+
+  scale_x_continuous(limits = c(4070000, 4140000), breaks = c(4.08e6, 4.1e6, 4.12e6, 4.14e6),label = c("4,080,000", "4,100,000", "4,120,000", "4,140,000"),position = "top")+
   scale_y_continuous(limits = c(0, 7))+
   theme_minimal() +  xlab("Genome Position (bp)") +
-  theme(legend.position = "none", axis.line = element_blank(), axis.text.y = element_blank(), 
+  theme(legend.position = "none", axis.line = element_blank(), axis.text.y = element_blank(), axis.title.y = element_blank(),
         panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank(),
-        axis.text = element_text(size=12), axis.title = element_text(size=12)) +
+        axis.text = element_text(size=12, color = "black"), axis.title = element_text(size=12)) +
   geom_rect(aes(xmin=4081494, xmax=4130628, ymin=0.5, ymax=1), fill="#4393C3")+
   geom_rect(aes(xmin=4071618, xmax=4112326, ymin=1.5, ymax=2), fill="#4393C3")+
   geom_text_repel(aes(x=Start, y=6), direction = "x", angle = 90, max.overlaps = Inf, min.segment.length = 0, fontface = "italic", nudge_y = -1.5, size = 3.6) 
@@ -212,8 +214,7 @@ snps_final %>%
   annotate(geom = "rect", xmin = 4086058-(18*3), xmax = 4086058-(159*3), ymin = 0, ymax = Inf, fill = "navy blue", alpha = 0.1) +
   annotate(geom = "rect", xmin = 4086058-(176*3), xmax = 4086058-(231*3), ymin = 0, ymax = Inf, fill = "#4393C3", alpha = 0.15) +
   theme_minimal() +  xlab("Genome Position") +
-  theme(legend.position = "none", panel.grid.minor = element_blank(), axis.text = element_text(size=12), axis.title = element_text(size=12))+
-  geom_text_repel(aes(x=Position, y=1), direction = "x", angle = 60, nudge_y =0.05, hjust= "left", min.segment.length = Inf, force = 0.1, size =3.6) 
+  theme(legend.position = "none", panel.grid.minor = element_blank(), axis.text = element_text(size=12, color = "black"), axis.title = element_text(size=12))+
 ggsave(filename= "Figure2a.2.pdf",dpi = 300, device="pdf", width = 8, height = 2)
 
 # Figure 2B
@@ -238,9 +239,8 @@ morA%>%
   scale_color_manual(values = c("none" = "dark gray", "PAS" = "DCE319FF", "DGC" = "#73D055FF", "PDE" = "#3CBB75FF")) +
   scale_x_continuous(limits = c(0, 1416))+
   scale_y_discrete(limits = c("3", '2', "1", "NA"), labels = c("Clinical Isolates", "Laboratory Isolates", "This Study", "NA")) +
-  theme_minimal() + theme(panel.grid.minor = element_blank(), text = element_text(size = 12))+ 
+  theme_minimal() + theme(panel.grid.minor = element_blank(), axis.text = element_text(size = 12, color = "black"))+ 
   xlab(element_blank()) + ylab(element_blank()) +
-  geom_text_repel(direction = "x", angle = 60, nudge_y =0.05, hjust= "left", min.segment.length = Inf, force = 0.1, size =3.4) +
   theme(legend.position="bottom", legend.box = "vertical") +
   guides(colour = guide_legend(override.aes = list(size=3))) +
   theme(legend.position = "none")+
@@ -251,6 +251,3 @@ morA%>%
   annotate(geom = "rect", xmin = 978, xmax = 1139, ymin = 0, ymax = Inf, fill = "#73D055FF", alpha = 0.2) +
   annotate(geom = "rect", xmin = 1159, xmax = 1394, ymin = 0, ymax = Inf, fill = "#3CBB75FF", alpha = 0.2) 
 ggsave(filename= "Figure2b.pdf", dpi=300, device = "pdf", width = 8, height = 4)
-
-# Figures were then cropped and combined to produce final versions
-# The snps_cast.csv and nje_cast.csv files were combined into Supplementary Data 1
